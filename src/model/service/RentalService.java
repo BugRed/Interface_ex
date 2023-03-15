2b2059ffe2923fb326aa075e2a2836cbb06340d7
Adriano Rodrigues <o.adrian.rodrigues@gmail.com>
2023-03-15 17:16:07 -0300

    Solution with interface

--- src/model/service/RentalService.java
+++ src/model/service/RentalService.java
@@ -1,7 +1,6 @@
 package model.service;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
 
 import model.entities.CarRental;
 import model.entities.Invoice;
@@ -11,9 +10,9 @@ public class RentalService {
 	private Double pricePerHour;
 	private Double pricePerDay;
 
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 
-	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
 		this.pricePerHour = pricePerHour;
 		this.pricePerDay = pricePerDay;
 		this.taxService = taxService;
